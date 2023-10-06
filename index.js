f5b24c36670dbf587deeb94ddb2865b6c3a6892e
Fir D. Husaini <92629226+Firdan9474@users.noreply.github.com>
2023-10-06 21:59:43 +0700

    push

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const app = express();
 const PORT = process.env.PORT || 3000;
-const baseURL = "https://be-semarang-19-production.up.railway.app/from";
+const baseURL = "/form";
 
 app.use(express.urlencoded({ extended: true }));
 app.set('view engine','ejs' );
